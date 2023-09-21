2d25cf00035d5b71cfd63e386cd61cab6a9a94a9
xiaolin <1591439872@qq.com>
2023-09-21 23:24:15 +0800

    产品详情

--- pages/goods/index.vue
+++ pages/goods/index.vue
@@ -122,6 +122,94 @@
 							</view>
 						</view>
 					</view>
+					<view class="tags">
+						<view class="group" v-for="(item,index) in 5" :key="index">
+							<text>超级好看</text>
+							<text>88</text>
+						</view>
+					</view>
+					<view class="evaluate_content">
+						<view class="album">
+							<view class="album__avatar">
+								<u-avatar :src="src"></u-avatar>
+							</view>
+							<view class="album__content">
+								<view class="user_info">
+									<view class="group">
+										<view class="" style="display: flex;">
+											<view class="name">
+												良松
+											</view>
+											<view class="tag">
+												再次回购
+											</view>
+										</view>
+										<view class="time">
+											2022.01.24
+										</view>
+									</view>
+									<view class="" style="margin-bottom: 20rpx;">
+										<u-rate :value="3.7" readonly></u-rate>
+									</view>
+								</view>
+								<view :style="{marginBottom: '8px',width: albumWidth + 'px'}">
+									<u--text text="太好看了，买来送朋友的"
+										style="color: #111;font-family: PingFang SC;font-size: 12px;font-weight: 400;"
+										:customStyle="{width: albumWidth + 'px'}"></u--text>
+								</view>
+								<u-album :urls="urls2" @albumWidth="width => albumWidth = width"
+									multipleSize="95"></u-album>
+							</view>
+						</view>
+					</view>
+				</view>
+
+			</view>
+			<view class="shop">
+				<view class="shop_head">
+					<view class="shop_head_info">
+						<view class="avatar">
+							<u-avatar src="/static/images/shop/shop.png"></u-avatar>
+						</view>
+						<view class="shop_head_name">
+							<view class="title">
+								乔丹旗舰店
+							</view>
+							<view class="score">
+								<view class="rate">
+									★★★★
+								</view>
+								<view class="time">
+									10年老店
+								</view>
+							</view>
+						</view>
+					</view>
+					<view class="shop_head_url">
+						<view :class="followed==true?'state active':'state'" @click="followedClick(true)">
+							<text v-if="!followed">关注</text>
+							<text v-if="followed">已关注</text>
+						</view>
+					</view>
+				</view>
+				<view class="shop_desc">
+					<view class="">
+						<text class="text">宝贝描述</text><text class="value">：4.0 低</text>
+					</view>
+					<view class="">
+						<text class="text">卖家服务</text><text class="value">：4.0 低</text>
+					</view>
+					<view class="">
+						<text class="text">物流服务</text> <text class="value">：4.0 低</text>
+					</view>
+				</view>
+				<view class="shop_tools">
+					<view class="text">
+						进店逛逛
+					</view>
+					<view class="text">
+						全部宝贝
+					</view>
 				</view>
 			</view>
 		</view>
@@ -136,7 +224,15 @@
 					'https://cdn.uviewui.com/uview/swiper/swiper2.png',
 					'https://cdn.uviewui.com/uview/swiper/swiper3.png',
 				],
-				pageIndex: 0
+				src: '/static/images/goods/avatar.png',
+				pageIndex: 0,
+				albumWidth: 0,
+				urls2: [
+					'https://cdn.uviewui.com/uview/album/1.jpg',
+					'https://cdn.uviewui.com/uview/album/2.jpg',
+					'https://cdn.uviewui.com/uview/album/3.jpg',
+				],
+
 			}
 		},
 		methods: {
@@ -413,6 +509,7 @@
 						font-family: PingFang SC;
 						font-size: 24rpx;
 						font-weight: 400;
+						padding-right: 58rpx;
 
 						text {
 							padding: 0 10rpx;
@@ -421,8 +518,8 @@
 
 					.icon {
 						image {
-						width: 14rpx;
-						height: 22rpx;
+							width: 14rpx;
+							height: 22rpx;
 						}
 					}
 				}
@@ -487,6 +584,7 @@
 						font-family: PingFang SC;
 						font-weight: 400;
 						font-size: 28rpx;
+
 						.data {
 							color: #939692;
 						}
@@ -499,23 +597,251 @@
 						font-family: PingFang SC;
 						font-size: 24rpx;
 						font-weight: 500;
-						.data{
+
+						.data {
 							color: #444942;
 							padding: 0 8rpx;
 							font-size: 28rpx;
 
 						}
+
 						.icon {
 							margin-top: 3rpx;
+
 							image {
 								width: 14rpx;
 								height: 22rpx;
 							}
 						}
 					}
+
 				}
+
+				.tags {
+					margin-top: 32rpx;
+					display: flex;
+					flex-wrap: wrap;
+
+					.group {
+						margin-right: 16rpx;
+						margin-bottom: 16rpx;
+						display: flex;
+						justify-content: space-between;
+						display: inline-flex;
+						max-width: 264rpx;
+						padding: 12rpx 16rpx;
+						border-radius: 9rpx;
+						border: 1px solid #F2F4F7;
+						font-family: PingFang SC;
+						font-weight: 400;
+
+						text:first-child {
+							color: #444942;
+							font-size: 24rpx;
+						}
+
+						text:last-child {
+							color: #6D746B;
+							font-size: 26rpx;
+						}
+					}
+				}
+
+				&_content {
+					.album {
+						@include flex;
+						align-items: flex-start;
+
+						&__avatar {
+							// background-color: $u-bg-color;
+							padding: 5px;
+							border-radius: 50%;
+						}
+
+						&__content {
+							margin-left: 10px;
+							flex: 1;
+						}
+					}
+
+					.user {
+						margin-top: 32rpx;
+						display: flex;
+
+						&_info {
+
+							.group {
+								display: flex;
+								font-weight: 400;
+								margin-bottom: 8rpx;
+								justify-content: space-between;
+
+								.name {
+									color: #333;
+									font-family: PingFang SC;
+									font-size: 28rpx;
+									margin-right: 8rpx;
+								}
+
+								.tag {
+									width: 122rpx;
+									height: 36rpx;
+									border-radius: 4rpx;
+									border: 1px solid #979797;
+									color: #999;
+									font-family: PingFang SC;
+									font-size: 22rpx;
+									font-weight: 400;
+									text-align: center;
+									line-height: 36rpx;
+									opacity: 0.2017;
+								}
+							}
+
+							.time {
+								color: #666;
+								font-family: PingFang SC;
+								font-size: 24rpx;
+								font-weight: 400;
+							}
+						}
+					}
+				}
+
+
+
 			}
 		}
 
+		.shop {
+			width: 94%;
+			margin: 0 auto;
+			height: 312rpx;
+			border-radius: 24rpx;
+			border: 1px solid rgba(255, 255, 255, 0.56);
+			background: #FFF;
+			backdrop-filter: blur(5.5px);
+			margin-top: 16rpx;
+			box-sizing: border-box;
+			padding: 32rpx 24rpx;
+
+			&_head {
+				display: flex;
+				justify-content: space-between;
+
+				&_info {
+					display: flex;
+
+					image {
+						background-color: #F2F4F7;
+						border-radius: 50%;
+						width: 80rpx;
+						height: 80rpx;
+					}
+
+					.title {
+						color: #000;
+						font-family: PingFang SC;
+						font-size: 32rpx;
+						font-weight: 400;
+
+						image {
+							margin-left: 10rpx;
+							width: 32rpx;
+							height: 32rpx;
+						}
+					}
+
+					.score {
+						display: flex;
+						align-items: center;
+
+						.time {
+							color: #6D746B;
+							text-align: center;
+							font-family: PingFang SC;
+							font-size: 24rpx;
+							font-weight: 400;
+							margin-left: 16rpx;
+						}
+					}
+				}
+
+				&_name {
+					margin-left: 24rpx;
+				}
+
+				&_url {
+					.state {
+						width: 144rpx;
+						height: 56rpx;
+						line-height: 56rpx;
+						text-align: center;
+						border-radius: 70rpx;
+						background: #F94E05;
+						color: #FFF;
+						font-family: PingFang SC;
+						font-size: 28rpx;
+					}
+
+					.active {
+						border-radius: 70rpx;
+						background: #0F1E0A;
+					}
+				}
+			}
+
+			&_desc {
+				display: flex;
+				margin: 32rpx 0 48rpx 0;
+				justify-content: space-around;
+				.text{
+					color:  #6D746B;
+					font-family: PingFang SC;
+					font-size: 20rpx;
+					font-weight: 400;
+				}
+				.value{
+					color: #000;
+					font-family: PingFang SC;
+					font-size: 24rpx;
+					font-weight: 400;
+				}
+				view{
+					position: relative;
+				}
+				view::after{
+					content: "";
+					display: block;
+					width: 2rpx;
+					height: 24rpx;
+					background-color: #DFE4DE;
+					position: absolute;
+					top: 50%;
+					transform: translateY(-50%);
+					right: -20rpx;
+				}
+			}
+			&_tools{
+				display: flex;
+				.text{
+					width: 50%;
+					text-align: center;
+					color: #000;
+					font-family: PingFang SC;
+					font-size: 28rpx;
+					font-weight: 400;
+					position: relative;
+				}
+				.text:first-child::after{
+					content: "";
+					position: absolute;
+					right: 0;
+					width: 1px;
+					height: 32rpx;
+					background-color:  #DFE4DE;
+				}
+			}
+		}
 	}
 </style>
