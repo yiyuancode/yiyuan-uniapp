528f13d7ec9cda4509b7688fed8716d9a1635b2c
Xiaolin <1591439872@qq.com>
2023-10-11 17:02:10 +0800

    更改接口

--- config/api/product.js
+++ config/api/product.js
@@ -1,6 +1,6 @@
 const http = uni.$u.http
 
-// get请求，获取商品分类列表
+// tab菜单分类，获取商品分类列表
 export const getProductCategory = (params, config = {}) => http.get('/ptm/productCategory/getTree')
 
 // get请求，获取菜单，注意：get请求的配置等，都在第二个参数中，详见前面解释
